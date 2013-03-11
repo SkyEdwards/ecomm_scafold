da598a4dc040f798af21d8810bfd33b3264e0867
Trynda E. Adair <tadair@academic.rrc.ca>
2013-03-11 15:11:22 -0500

    msg

---
+++ ecomm_scafold/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EcommScafold::Application.config.secret_token = '0059fe426b161c9fd6164dcd958b5dfec422678e0c7e37d88c22c0bdc5c0fde9bcdfd3f533f1a42865a6d6ab9b6cc100469f7a660cc52c8da99d295e4d727dcc'
